8509a4c17c60f0091d1460473cb61bf9606a9544
Alan Hecht <alanghecht@gmail.com>
2025-03-17 12:41:03 -0400

    gem skeleton

---
+++ lib/health_monitor.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "health_monitor/version"
+
+module HealthMonitor
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/health_monitor_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe HealthMonitor do
+  it "has a version number" do
+    expect(HealthMonitor::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
