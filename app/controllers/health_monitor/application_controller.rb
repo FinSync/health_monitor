9bceaa41f1e61bb89d65d0bc94fc087801e577b6
Alan Hecht <alanghecht@gmail.com>
2025-03-17 15:49:38 -0400

    Add a HTTP API token to prevent unauthorized access

--- app/controllers/health_monitor/application_controller.rb
+++ app/controllers/health_monitor/application_controller.rb
@@ -1,4 +1,11 @@
 module HealthMonitor
-  class ApplicationController < ActionController::Base
+  class ApplicationController < ActionController::API
+    before_action :check_api_token
+
+    def check_api_token
+      if ENV['HEALTH_MONITOR_API_TOKEN'].present? && (request.headers['HTTP_API_TOKEN'] != ENV['HEALTH_MONITOR_API_TOKEN'])
+        render json: 'unauthorized', status: :unauthorized
+      end
+    end
   end
 end
--- app/models/health_monitor/application_record.rb
+++
@@ -1,5 +0,0 @@
-module HealthMonitor
-  class ApplicationRecord < ActiveRecord::Base
-    self.abstract_class = true
-  end
-end
