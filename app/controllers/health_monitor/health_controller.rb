8ce9183b12353d3ed5ce5650824da266d80cc61a
Alan Hecht <alanghecht@gmail.com>
2025-03-19 22:39:57 -0400

    set the return status to ok

--- app/controllers/health_monitor/health_controller.rb
+++ app/controllers/health_monitor/health_controller.rb
@@ -2,7 +2,7 @@ module HealthMonitor
   class HealthController < ApplicationController
     def index
       status = HealthCheckService.new.call
-      render json: status
+      render json: status, status: :ok
     end
   end
 end
