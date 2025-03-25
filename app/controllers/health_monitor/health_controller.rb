afea70674a8a8f52472485d93e7175b6083e0d71
Alan Hecht <alanghecht@gmail.com>
2025-03-25 14:14:10 -0400

    expire

--- app/controllers/health_monitor/health_controller.rb
+++ app/controllers/health_monitor/health_controller.rb
@@ -2,6 +2,11 @@ module HealthMonitor
   class HealthController < ApplicationController
     def index
       status = HealthCheckService.new.call
+
+      response.set_header('Cache-Control', 'no-cache, no-store, must-revalidate')
+      response.set_header('Pragma', 'no-cache')
+      response.set_header('Expires', '0')
+
       render json: status, status: :ok
     end
   end
