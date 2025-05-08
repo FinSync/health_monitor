075697712d8bcbfcd219a6ef2485ddedd1abe5fc
Alan Hecht <alanghecht@gmail.com>
2025-05-08 09:39:25 -0400

    Another attempt at port re-routing

--- app/controllers/health_monitor_lb/application_controller.rb
+++ app/controllers/health_monitor_lb/application_controller.rb
@@ -3,6 +3,12 @@ module HealthMonitorLb
     before_action :check_api_token
 
     def check_api_token
+      class << request
+        def port
+          80
+        end
+      end
+
       if ENV['HEALTH_MONITOR_API_TOKEN'].present? && (params['API_TOKEN'] != ENV['HEALTH_MONITOR_API_TOKEN'])
         render json: 'unauthorized', status: :unauthorized
       end
--- app/controllers/health_monitor_lb/health_controller.rb
+++ app/controllers/health_monitor_lb/health_controller.rb
@@ -1,7 +1,5 @@
 module HealthMonitorLb
   class HealthController < ApplicationController
-    before_action :disable_ssl, only: :disable_ssl_actions
-
     def index
       status = HealthCheckService.new.call
 
@@ -10,18 +8,6 @@ module HealthMonitorLb
       response.set_header('Expires', '0')
 
       render json: status, status: :ok
-    end
-
-    def disable_ssl
-      # This is a workaround for the issue with SSL termination in AWS ELB
-      # where the health check URL is not accessible over HTTP.
-      redirect_to request.url.gsub(/https/i, 'http') if request.ssl?
-    end
-
-    def disable_ssl_actions
-      actions = []
-      actions << :index unless ENV['ENABLE_HEALTH_CHECK_SSL']
-      actions
     end
   end
 end
